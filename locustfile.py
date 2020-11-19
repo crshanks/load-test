69eed9da2f38505324a66d8d449cb52d83a531d1
Craig Shanks <cshanks@newrelic.com>
2020-11-19 17:24:59 +0000

    Update to locust 1.0+

--- locustfile.py
+++ locustfile.py
@@ -1,21 +1,25 @@
 import base64
 
-from locust import HttpLocust, TaskSet, task
+from locust import HttpUser, TaskSet, task, between
 from random import randint, choice
 
 
-class WebTasks(TaskSet):
+class WebTasks(HttpUser):
+    wait_time = between(1, 2)
 
     @task
     def load(self):
-        base64string = base64.encodestring('%s:%s' % ('user', 'password')).replace('\n', '')
+#        base64string = base64.encodebytes(bytes(('%s:%s' % ('user', 'password')).replace('\n', ''), encoding = 'utf-8'))
+        usrPass = "user:password"
+#        b64Val = base64.b64encode(usrPass)
+        encoded_u = base64.b64encode(usrPass.encode()).decode()
 
         catalogue = self.client.get("/catalogue").json()
         category_item = choice(catalogue)
         item_id = category_item["id"]
 
         self.client.get("/")
-        self.client.get("/login", headers={"Authorization":"Basic %s" % base64string})
+        self.client.get("/login", headers={"Authorization":"Basic %s" % encoded_u})
         self.client.get("/category.html")
         self.client.get("/detail.html?id={}".format(item_id))
         self.client.delete("/cart")
@@ -24,7 +28,8 @@ class WebTasks(TaskSet):
         self.client.post("/orders")
 
 
-class Web(HttpLocust):
-    task_set = WebTasks
-    min_wait = 0
-    max_wait = 0
+# class Web(HttpUser):
+    
+#     task_set = WebTasks
+#     min_wait = 0
+#     max_wait = 0
